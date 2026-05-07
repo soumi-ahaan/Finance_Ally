bb4304e241de89f0274248fe74dffa4124c5e42e
MaitySoumitra <soumitra.ahaansoftware@gmail.com>
2026-05-07 17:38:54 +0530

    HeroBanner and StatBanner

--- src/components/pages/Home/HomeComponent.tsx
+++ src/components/pages/Home/HomeComponent.tsx
@@ -1,14 +1,19 @@
 import AudienceSection from "./AudienceSection"
 import CallToAction from "./CallToAction"
+import { HeroBanner } from "./HeroBanner"
 import OurServices from "./OurServices"
+import { StatBanner } from "./StatBanner"
 
 
 const HomeComponent = () => {
   return (
     <div>
+      <HeroBanner/>
+       <StatBanner/>
         <OurServices/>
         <AudienceSection/>
         <CallToAction/>
+       
     </div>
   )
 }
